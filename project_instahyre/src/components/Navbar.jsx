1f31b41dd11352dd3e6b1b93229605b7a7ecf3f5
Abhishek <9528442373.as@gmail.com>
2021-07-21 10:43:08 +0530

    created instahyre react app and included a router in navbar

---
+++ project_instahyre/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+const Navbar = ({prop}) => {
+    return (
+        <div>
+            <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/signup">SignUp</Link>
+      </li>
+    </ul>
+    <Switch>
+              <Route exact path='/' component={prop.Home}></Route>
+              <Route exact path='/login' component={prop.Login}></Route>
+              <Route exact path='/signup' component={prop.Signup}></Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default Navbar
