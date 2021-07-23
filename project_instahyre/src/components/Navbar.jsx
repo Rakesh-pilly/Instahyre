e7d7857b27dcefaa046ff92ab877d12f1a4d377c
Milind-dev <milinddev1011@gmail.com>
2021-07-23 23:56:22 +0530

    folder Oppturniteies ,,,, Oppturnities , modulecss and oppturnities

--- project_instahyre/src/components/Navbar.jsx
+++ project_instahyre/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ import Signup from "./Signup";
 import Navcandidate from "./Navcandidate"
 import Home from "./Home";
 
+import OpportunityViews from './Opportunities/OpportunityViews'
+import Opportunities from './Opportunities/Opportunities';
 
 const Navbar = () => {
   const [signup,setSignup] = useState('');
@@ -35,14 +37,22 @@ console.log(l,s)
       <li>
         <Link to="/signup">SignUp</Link>
       </li>
+      <li>
+        <Link to="/Opportunities">Oppturnities</Link>
+      </li>
+      <li>
+        <Link to="/OpportunityViews">OpportunityViews</Link>
+      </li>
      
     </ul>
     <Switch>
               <Route exact path='/' component={Home}></Route>
-              
               <Route exact path='/login' render={() => <Login prop={(x) => handleLogin(x)}  title={`Props through render`} />}></Route>
               <Route exact path='/signup' render={() => <Signup prop={(x) => handleSignup(x)}  title={`Props through render`} />}></Route>
+              <Route exact path='/Opportunities' render={()=><Opportunities prop={x=>handleSignup(x)} title={`props through render`} />}></Route>
+              <Route exact path='/OpportunityViews' render={()=><OpportunityViews prop={x=>handleSignup(x)} title={`props through render`} />}></Route>
             </Switch>
+      
         </div>
     )
 
@@ -55,6 +65,10 @@ console.log(l,s)
         return <Navcandidate token={[(x) => handleLogin(x),(x) => handleSignup(x)]}/>
       }
     }
+    
 }
+
+// oppturnities
+
 
 export default Navbar
---
+++ project_instahyre/src/components/Opportunities/OpportunityViews.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import styles from "../Opportunities/Opportunity.module.css"
+
+const OpportunityViews = () => {
+    const [jobs, setJobs] = useState('');
+
+    if (!jobs) {
+        axios.get('http://localhost:3001/jobs').then(res => { console.log(res, 'res'); setJobs(res.data) })
+    }
+
+    if (jobs) {
+        return jobs.map((el,i) => {
+            var { responsibilities,skills, about, location, Function} = el
+            return (
+                <div className={styles.oppview__box}>
+                    <div className={styles.oppview__box2}>
+                        <div>
+                            <img className={styles.opp__logo} src={el.logo} alt="" />
+                        </div>
+                        <div>
+                            <h2 className={styles.opp__role}> {el.role} </h2>
+                            <p className={styles.location}> {el.company} </p>
+                            <p className={styles.location}> {el.location} </p>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 className={styles.oppview__descriptions}> JOB DESCRIPTION </h1>
+                        <p> <span className={styles.oppview__head} > {skills}  </span> </p>
+                        <p> <span className={styles.oppview__head}> Location :  {location} </span></p>
+                        <p> <span className={styles.oppview__head}> Function : {Function} </span></p>
+                        
+                            <h4 className={styles.oppview__descriptions}>Responsiblity</h4>
+                        <ul className={styles.opp__ul}>    
+                            <li>{responsibilities[0]}</li>
+                            <li>{responsibilities[1]}</li>
+                            <li>{responsibilities[2]}</li>
+                        </ul>
+
+                        <h1 className={styles.oppview__descriptions}> ABOUT </h1>
+                        <p> <span className={styles.oppview__head}> {about} </span> </p>
+                    </div>
+                    <div className={styles.opp__productphotos}> 
+                           <h1 className={styles.oppview__descriptions}> Infozzle Office and Product Photos </h1>
+                           <div className={styles.oppview__images}> 
+                                <div>
+                                    <img className={styles.opp__logo} src={el.logo} alt=""/>
+                                </div>
+                                <div>
+                                    <img className={styles.opp__logo} src={el.logo} alt=""/>
+                                </div>
+                           </div>
+                    </div>
+                </div>
+
+            )
+        })
+    }
+    return <h1>Fetching...</h1>
+
+}
+
+export default OpportunityViews
