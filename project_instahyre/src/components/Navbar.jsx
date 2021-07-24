3241550b9650afd7b8f8bbb5ffa4f4ad93320a6f
Abhishek <9528442373.as@gmail.com>
2021-07-24 09:21:55 +0530

    updated Nav

--- project_instahyre/src/components/Navbar.jsx
+++ project_instahyre/src/components/Navbar.jsx
@@ -1,60 +1,137 @@
-import React, { useState } from 'react'
-import {Route, Link, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { Route, Link, Switch } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
-import Navcandidate from "./Navcandidate"
+import Navcandidate from "./Navcandidate";
 import Home from "./Home";
+import styled from "styled-components";
 
+const Space = styled.div`
+height:50px;
+`;
+const Navbox = styled.nav`
+position:fixed;
+top:0%;
+z-index:100;
+
+*{
+  color:white;
+  a{
+    text-decoration:none;
+  }
+}
+  width: 100%;
+  height: 50px;
+  background-color: rgb(65, 70, 84);
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  div{
+    margin-left:50px;
+    img{
+      width:100px;
+    }
+  }
+`;
+const Navul = styled.ul`
+display:inline-flex;
+list-style:none;
+margin-right:50px;
+`;
+const Navli = styled.li`
+padding:19px 15px;
+letter-spacing:2px;
+font-weight:bold;
+font-size:10px;
+&:hover{
+  background-color:rgba(255,255,255,.1);
+  cursor:pointer;
+}
+span{
+  border-radius:5px;
+  padding:5px;
+  background-color:rgb(2,191,160);
+}
+`;
 
 const Navbar = () => {
-  const [signup,setSignup] = useState('');
-  const [login,setLogin] = useState('');
-  console.log('refreshed')
+  const [signup, setSignup] = useState("");
+  const [login, setLogin] = useState("");
+  console.log("refreshed");
   const handleSignup = (x) => {
     setSignup(x);
-  }
+  };
   const handleLogin = (x) => {
     setLogin(x);
-  }
-  
-  
+  };
 
-  let l = localStorage.getItem('login');
-  let s = localStorage.getItem('signup');
-console.log(l,s)
-    if(!l && !s){
+  let l = localStorage.getItem("login");
+  let s = localStorage.getItem("signup");
+
+  if (!l && !s) {
+    return (
+      <div>
+        <Space>
+        <Navbox>
+          <div>
+            <Link to="/"><img src="https://static.instahyre.com/images/logos/logo.png" alt="Instahyre" /></Link>
+          </div>
+          <Navul>
+            
+          <Navli>
+           INVITE FRIENDS
+          </Navli>
+            <Navli>
+              CUSTOMERS
+            </Navli>
+            
+              <Link to="/login"><Navli>LOGIN</Navli></Link>
+            
+            
+              <Link to="/signup"><Navli>SIGNUP</Navli></Link>
+            
+            <Navli>
+              <span>EMPLOYERS</span>
+            </Navli>
+          </Navul>
+        </Navbox>
+        </Space>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+
+          <Route
+            exact
+            path="/login"
+            render={() => (
+              <Login
+                prop={(x) => handleLogin(x)}
+                title={`Props through render`}
+              />
+            )}
+          ></Route>
+          <Route
+            exact
+            path="/signup"
+            render={() => (
+              <Signup
+                prop={(x) => handleSignup(x)}
+                title={`Props through render`}
+              />
+            )}
+          ></Route>
+        </Switch>
+      </div>
+    );
+  } else {
+    if (l) {
+      return <Navcandidate token={"login"} />;
+    }
+    if (s) {
       return (
-        <div>
-            <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/signup">SignUp</Link>
-      </li>
-     
-    </ul>
-    <Switch>
-              <Route exact path='/' component={Home}></Route>
-              
-              <Route exact path='/login' render={() => <Login prop={(x) => handleLogin(x)}  title={`Props through render`} />}></Route>
-              <Route exact path='/signup' render={() => <Signup prop={(x) => handleSignup(x)}  title={`Props through render`} />}></Route>
-            </Switch>
-        </div>
-    )
-
+        <Navcandidate token={[(x) => handleLogin(x), (x) => handleSignup(x)]} />
+      );
     }
-    else{
-      if(l){
-        return <Navcandidate token={'login'} />
-      }
-      if(s){
-        return <Navcandidate token={[(x) => handleLogin(x),(x) => handleSignup(x)]}/>
-      }
-    }
-}
+  }
+};
 
-export default Navbar
+export default Navbar;
--- project_instahyre/src/components/Navcandidate/Candidatenav.jsx
+++ project_instahyre/src/components/Navcandidate/Candidatenav.jsx
@@ -2,6 +2,55 @@
 import { Route, Link, Switch, withRouter} from "react-router-dom";
 import Opportunities from "../Opportunities/Opportunities";
 import Jobdetail from "../Opportunities/Jobdetail";
+import styled from "styled-components";
+
+const Space = styled.div`
+height:50px;
+`;
+
+const Navbox = styled.nav`
+position:fixed;
+top:0%;
+z-index:100;
+*{
+  color:white;
+  a{
+    text-decoration:none;
+  }
+}
+  width: 100%;
+  height: 50px;
+  background-color: rgb(65, 70, 84);
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  div{
+    margin-left:50px;
+    img{
+      width:100px;
+    }
+  }
+`;
+const Navul = styled.ul`
+display:inline-flex;
+list-style:none;
+margin-right:50px;
+`;
+const Navli = styled.li`
+padding:19px 15px;
+letter-spacing:2px;
+font-weight:bold;
+font-size:10px;
+&:hover{
+  background-color:rgba(255,255,255,.1);
+  cursor:pointer;
+}
+span{
+  border-radius:5px;
+  padding:5px;
+  background-color:rgb(2,191,160);
+}
+`;
 
 const Candidatenav = withRouter(({history}) => {
     const handleSignout = (e) => {
@@ -12,20 +61,26 @@ const Candidatenav = withRouter(({history}) => {
 
     return (
         <div>
-             
-                
-             <ul>
-      <li>
-        <Link to="/candidate/Opportunities">Instahyre</Link>
-      </li>
-      <li>
-        <Link to="/candidate/job">Opportunities</Link>
-      </li>
-      <li>
-        <button onClick={e => handleSignout(e)}>SignOut</button>
-      </li>
-    </ul>
+          <Space>
+          <Navbox>
+           <div>
+            <Link to="/candidate/Opportunities"><img src="https://static.instahyre.com/images/logos/logo.png" alt="Instahyre" /></Link>
+          </div>
+
+               <Navul>
+               <Navli>
+              <span>GO PREMIUM</span>
+            </Navli>
+      <Navli>ACTIVITY</Navli>
+      
+        <Link to="/candidate/job"><Navli>OPPORTUNITIES</Navli></Link>
+        <Navli>PROFILE</Navli>
+        <Navli>SETTINGS</Navli>
+      <Navli onClick={e => handleSignout(e)}>SIGN OUT</Navli>
+               </Navul>
+           </Navbox>
     
+          </Space>
     <Switch>
     
               <Route exact path='/candidate/Opportunities' component={Opportunities} ></Route>
